ec2d51b17c198c6f9dd27534262ef197be54d9e7
David Fernando Corbetta <david.corbetta@db1.com.br>
2017-03-08 11:10:45 -0300

    aep01 de programação avançada

---
+++ faculdade/programacao-avancada/1-bimestre/aep/aep-1.java
@@ -0,0 +1,21 @@
+
+public class Aep01 {
+
+	public static void main(String[] args) {
+		double notaA;
+		double notaB;
+		double notaC;
+		double media;
+		
+		notaA = 8;
+		notaB = 3;
+		notaC = 7;
+		
+		media = (notaA + notaB + notaC) /3;
+		
+		System.out.print(media);
+		
+
+	}
+
+}
